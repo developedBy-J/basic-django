a922cac05391667af768a1fe2bed7b35d30d692e
Akash Kumar <82937540+developedBy-J@users.noreply.github.com>
2021-06-13 14:53:58 +0530

    Update admin.py

--- watchlist_app/admin.py
+++ watchlist_app/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
 from watchlist_app.models import WatchList, StreamPlatform, Review
-# Register your models here.
 
 admin.site.register(WatchList)
 admin.site.register(StreamPlatform)
